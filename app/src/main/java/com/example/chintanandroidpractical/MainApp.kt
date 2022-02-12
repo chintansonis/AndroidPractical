c0d0f9c80818657a6aa129da0c23f82e7a2405fe
chintansonis <cvsoni22@gmail.com>
2022-02-12 12:51:44 +0000

    Added base and initial required dependencies for project.

---
+++ app/src/main/java/com/example/chintanandroidpractical/MainApp.kt
@@ -0,0 +1,12 @@
+package com.example.chintanandroidpractical
+
+import android.app.Application
+
+
+
+class MainApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/example/chintanandroidpractical/ui/LauncherMainActivity.kt
+++ app/src/main/java/com/example/chintanandroidpractical/ui/LauncherMainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.chintanandroidpractical
+package com.example.chintanandroidpractical.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,7 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.chintanandroidpractical.ui.theme.ChintanAndroidPracticalTheme
 
-class MainActivity : ComponentActivity() {
+class
+MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
